a0e2918f3027cad0b0198071ebd8bdd71449fe76
haru0sameee <it232183@trident.ac.jp>
2024-01-14 01:15:15 +0900

    ステージお試し　タイルマップ

--- Assets/tuji/LiftGimmick.cs
+++ Assets/tuji/LiftGimmick.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+/// <summary>
+/// ƒŠƒtƒg‚Ìã‰º
+/// </summary>
+public class LiftGimmick : MonoBehaviour
 {
+
+    //À•W
+    private Vector2 m_pos;
+
+    //
+    private Vector2 m_vel;
+
     // Start is called before the first frame update
     void Start()
     {
