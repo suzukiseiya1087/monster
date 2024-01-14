ed2a6985f85990d45cf7cf72b44f464f7a91d646
haru0sameee <it232183@trident.ac.jp>
2024-01-15 00:52:03 +0900

    監視カメラギミック追加　リフト作成中

--- Assets/tuji/LiftGimmick.cs
+++ Assets/tuji/LiftGimmick.cs
@@ -7,42 +7,39 @@ using UnityEngine;
 /// </summary>
 public class LiftGimmick : MonoBehaviour
 {
+    private Transform targetPosition1; // 上昇する位置
+    private Transform targetPosition2; // 下降する位置
+    [SerializeField] float speed = 2.0f; // エレベーターの速さ
+    private float waitingTime = 2.0f; // リフトが待機する時間
 
-    public class Elevator : MonoBehaviour
+    private bool isMoving = false;
+
+    void Start()
     {
-        private Transform targetPosition1; // 上昇する位置
-        private Transform targetPosition2; // 下降する位置
-        [SerializeField] float speed = 2.0f; // エレベーターの速さ
-        private float waitingTime = 2.0f; // リフトが待機する時間
 
-        private bool isMoving = false;
+        StartCoroutine(MoveElevator());
+    }
 
-        void Start()
+    IEnumerator MoveElevator()
+    {
+        while (true)
         {
-            
-            StartCoroutine(MoveElevator());
-        }
-
-        IEnumerator MoveElevator()
-        {
-            while (true)
-            {
-                yield return StartCoroutine(MoveToPosition(targetPosition1.position)); // 上昇
-                yield return new WaitForSeconds(waitingTime);
-                yield return StartCoroutine(MoveToPosition(targetPosition2.position)); // 下降
-                yield return new WaitForSeconds(waitingTime);
-            }
-        }
-
-        IEnumerator MoveToPosition(Vector3 target)
-        {
-            isMoving = true;
-            while (transform.position != target)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-                yield return null;
-            }
-            isMoving = false;
+            yield return StartCoroutine(MoveToPosition(targetPosition1.position)); // 上昇
+            yield return new WaitForSeconds(waitingTime);
+            yield return StartCoroutine(MoveToPosition(targetPosition2.position)); // 下降
+            yield return new WaitForSeconds(waitingTime);
         }
     }
+
+    IEnumerator MoveToPosition(Vector3 target)
+    {
+        isMoving = true;
+        while (transform.position != target)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            yield return null;
+        }
+        isMoving = false;
+    }
 }
+
